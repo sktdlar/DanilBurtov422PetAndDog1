1cbca0107efb8adf0a6f920132acae74153da033
222103 <222103@WINMAC2>
2025-02-27 12:44:46 +0300

    Добавьте файлы проекта.

---
+++ DanilBurtov422PetAndDog/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using DanilBurtov422PetAndDog.Components.DataBase;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DanilBurtov422PetAndDog
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static DanilBurtov422_PetsEntities db = new DanilBurtov422_PetsEntities(); 
+    } 
+}
